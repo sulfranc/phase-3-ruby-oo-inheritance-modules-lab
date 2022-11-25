b7bcd69df6d9a548ba7b7f2703fb355bd3061f84
sulfranc <sulfranc@gmail.com>
2022-11-25 12:52:55 -0500

    done

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,8 +5,6 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  extend memorable
-
   @@artists = []
 
   def initialize
@@ -22,9 +20,13 @@ class Artist
     @@artists
   end
 
-  
+  def self.reset_all
+    self.all.clear
+  end
 
-  
+  def self.count
+    self.all.count
+  end
 
   def add_song(song)
     @songs << song
@@ -35,7 +37,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  #def to_param
-    #name.downcase.gsub(' ', '-')
-  #end
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
 end
